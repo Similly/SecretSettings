8b04dd02cd808040a404e9954ae84c2a675e6878
Similly <wand.simon1@gmail.com>
2022-11-27 21:33:45 +0100

    Initial Commit

---
+++ SecretSettings/SecretSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretSettingsApp.swift
+//  SecretSettings
+//
+//  Created by Simon Wand on 27.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SecretSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
